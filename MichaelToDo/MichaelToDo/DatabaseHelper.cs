3f3e29117b30867df5fd85f2474a2e48f60bc0e4
Harugot <33010386+Harugot@users.noreply.github.com>
2019-03-25 18:25:41 -0500

    ToDo App

---
+++ MichaelToDo/MichaelToDo/DatabaseHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using System.Threading.Tasks;
+
+namespace MichaelToDo
+{
+    public class DatabaseHelper
+    {
+        readonly SQLiteAsyncConnection database;
+
+        //Constructor that will create a dbHelper with a device specific path
+        public DatabaseHelper(string path)
+        {
+            database = new SQLiteAsyncConnection(path);
+            database.CreateTableAsync<Task>().Wait();
+        }
+
+        //Returns a list of all available tasks asyncrononmously.
+        public Task<List<Task>> GetItemsAsync()
+        {
+            return database.Table<Task>().ToListAsync();
+        }
+
+        //Makes a query to the database asking for all Tasks where Done is false/0;
+        public Task<List<Task>> GetItemsNotDoneAsync()
+        {
+            return database.QueryAsync<Task>("SELECT * FROM [Task] WHERE [Done] = 0");
+        }
+
+        //Returns a task by its ID - will return null if none is found.
+        public Task<Task> GetItemAsync (int id)
+        {
+            return database.Table<Task>().Where(i => i.ID == id).FirstOrDefaultAsync();
+        }
+
+        //Inserts or updates the table depending if the tasks ID already exists in the table.
+        public Task<int> SaveItemAsync(Task task)
+        {
+            if (task.ID != 0)
+            {
+                return database.UpdateAsync(task);
+            }else
+            {
+                return database.InsertAsync(task);
+            }
+        }
+
+        //Deletes a task from the table basd on its ID.
+        public Task<int> DeleteItemAsync(Task task)
+        {
+            return database.DeleteAsync(task);
+        }
+    }
+}
---
+++ MichaelToDo/MichaelToDo/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace MichaelToDo
+{
+    public class Task
+    {
+        [PrimaryKey]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Date { get; set; }
+
+        public bool Done { get; set; }
+    }
+}
